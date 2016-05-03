eb711485ab44b193d2cbbf76f03702ee34418560
Christian Fobel <christian@fobel.net>
2016-05-03 11:42:07 -0400

    Bump required wheeler.pygtkhelpers version

--- __init__.py
+++ __init__.py
@@ -168,8 +168,8 @@ class DropbotDxPlugin(Plugin, StepOptionsController):
         '''
         config = self.dropbot_dx_remote.config
         form = dict_to_form(config)
-        dialog = FormViewDialog('Edit configuration settings')
-        valid, response = dialog.run(form)
+        dialog = FormViewDialog(form, 'Edit configuration settings')
+        valid, response = dialog.run()
         if valid:
             self.dropbot_dx_remote.update_config(**response)
 
@@ -198,8 +198,8 @@ class DropbotDxPlugin(Plugin, StepOptionsController):
                               properties={'patterns':
                                           [('Dstat parameters file (*.yml)',
                                             ('*.yml', ))]}))
-        dialog = FormViewDialog()
-        valid, response = dialog.run(form)
+        dialog = FormViewDialog(form, 'Set DStat parameters file')
+        valid, response = dialog.run()
 
         if valid:
             options['dstat_params_file'] = response['dstat_params_file']
